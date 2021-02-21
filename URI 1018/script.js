255fd8253c6154109f49751bc0c2775cf247a315
Jonathan Engel <joow.engeel@gmail.com>
2021-02-21 11:42:10 -0300

    Primeiro estágio do URI 1018

---
+++ URI 1018/script.js
@@ -0,0 +1,53 @@
+// PROBLEMA: 1018 - Cédulas
+// RESPOSTA: Accepted
+
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+let valor = parseFloat(lines.shift());
+
+let cem=0, cinquenta=0, vinte=0, dez=0, cinco=0, dois=0, um=0;
+let resto;
+
+if(valor>=100){
+    cem = Math.floor(valor/100);
+    resto = valor%100;
+    valor = resto;
+}
+if(valor>=50){
+    cinquenta = Math.floor(valor/50);
+    resto = valor%50;
+    valor =resto;
+}
+if(valor>=20){
+    vinte = Math.floor(valor/20);
+    resto = valor%20;
+    valor =resto;
+}
+
+if(valor>= 10){
+    dez = Math.floor(valor/10);
+    resto = valor%10;
+    valor =resto;
+}
+
+if(valor >=5){
+    cinco = Math.floor(valor/5);
+    resto = valor%5;
+    valor =resto;
+}
+if(valor >=2){
+    dois = Math.floor(valor/2);
+    resto = valor%2;
+    valor =resto;
+}
+
+if(valor>=1){
+    um = Math.floor(valor/1);
+    resto = valor%1;
+    valor =resto;
+}
+
+let resultado = `${valor}\n${cem} nota(s) de R$ 100,00\n${cinquenta} nota(s) de R$ 50,00\n${vinte} nota(s) de R$ 20,00\n${dez} nota(s) de R$ 10,00\n${cinco} nota(s) de R$ 5,00\n${dois} nota(s) de R$ 2,00\n${um} nota(s) de R$ 1,00`;
+
+console.log(resultado);
