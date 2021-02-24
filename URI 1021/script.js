7b3b3a2b776f4d66c5eb235b4789b931477b7cf8
Jonathan Engel <joow.engeel@gmail.com>
2021-02-24 19:15:38 -0300

    ajuste do uri 1021 realizado e funcionando com sucesso!

--- URI 1021/script.js
+++ URI 1021/script.js
@@ -2,27 +2,30 @@
 // RESPOSTA: Accepted
 
 var input = require("fs").readFileSync("stdin", "utf8");
-var lines = input.split("\n");
+var lines = input.split(".");
 
-let valor = parseFloat(lines.shift());
+let intNota = parseInt(lines.shift());
+let intMoeda = parseInt(lines.shift());
 
 let valorNotas = [100, 50, 20, 10, 5, 2];
 let qtdNotas = new Array(valorNotas.length);
 console.log("NOTAS:");
 
 for (let i = 0; i < qtdNotas.length; i++) {
-    qtdNotas[i] = Math.floor(valor / valorNotas[i]);
-    valor = valor % valorNotas[i];
+    qtdNotas[i] = Math.floor(intNota / valorNotas[i]);
+    intNota = intNota % valorNotas[i];
     console.log( qtdNotas[i] + " nota(s) de R$ " + valorNotas[i].toFixed(2));
 }
 
-let valorMoedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01];
+intMoeda += (intNota*100);
+let valorMoedas = [100, 50, 25, 10, 5, 1];
 let qtdMoedas = new Array(valorMoedas.length);
 
 console.log("MOEDAS:");
 for (let i = 0; i < qtdMoedas.length; i++) {
-    qtdMoedas[i] = Math.floor(valor / valorMoedas[i]);
-    valor = valor % valorMoedas[i];
+    qtdMoedas[i] = Math.floor(intMoeda / valorMoedas[i]);
+    intMoeda = intMoeda % valorMoedas[i];
     
-    console.log(qtdMoedas[i] + " moeda(s) de R$ " + valorMoedas[i].toFixed(2));
+    console.log(qtdMoedas[i] + " moeda(s) de R$ " + (valorMoedas[i]/100).toFixed(2));
 }
+
